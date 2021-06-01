0c5f28eb35c49402400db7720fd1a0c511974950
raiym <79152585+SamT002@users.noreply.github.com>
2021-06-01 14:22:18 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/test_5/MainActivity.kt
+++ app/src/main/java/com/example/test_5/MainActivity.kt
@@ -11,7 +11,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var insert_name:EditText
     lateinit var greet:Button
     lateinit var name:TextView
-
+    
+    if (savedInstanceState!= null){
+            val a = savedInstanceState.getString("name")
+            name.text = "Hello, $a"
+        }
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +29,9 @@ class MainActivity : AppCompatActivity() {
         greet.setOnClickListener {
             name.text = "Hello, ${insert_name.text.trim()}"
         }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("name", insert_name.text.toString().trim())
     }
 }
